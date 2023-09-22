70bd1392e270c07a4848adae35aed867c000bc92
Olga B <orlett@yandex.ru>
2023-09-22 18:49:08 +0300

    add new files

---
+++ _init_.py
@@ -0,0 +1,6 @@
+from task7_01 import num_add
+from task7_2 import name_file
+from task7_4 import  new_file
+from hw7_1 import group_rename
+
+__all__ = ['num_add', 'name_file', 'new_file', 'group_rename']
